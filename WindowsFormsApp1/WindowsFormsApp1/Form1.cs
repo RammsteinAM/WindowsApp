d1c2f7733cefc57c63c9886e330a84e3caf961aa
Awet Brutjan <rammsteinam@gmail.com>
2017-05-17 23:08:43 +0400

    1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = this.username.Text;
+            string password = this.password.Text;
+            bool isChecked = this.iAgree.Checked;
+
+            if (username == "admin" && password == "admin" && isChecked)
+            {
+                MessageBox.Show("Բարեւ, ադմին։");
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Տարոն ջան, սիրուն չի։");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button1.Width += 7;
+        }
+    }
+}
