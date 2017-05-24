41802d3b8534f9c54b311e332c31e6898c9a69fc
Awet Brutjan <RammsteinAM@Gmail.com>
2017-05-24 18:03:52 +0400

    24.05.2017

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp1
 {
-    partial class login
+    partial class loginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,110 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
             this.button1 = new System.Windows.Forms.Button();
             this.username = new System.Windows.Forms.TextBox();
             this.password = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.iAgree = new System.Windows.Forms.CheckBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.usernameOK = new System.Windows.Forms.PictureBox();
+            this.passwordOK = new System.Windows.Forms.PictureBox();
+            this.usernameWrong = new System.Windows.Forms.PictureBox();
+            this.passwordWrong = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.usernameOK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordOK)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usernameWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordWrong)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(101, 215);
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(79, 120);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Խառաշո";
+            this.button1.Size = new System.Drawing.Size(126, 154);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Մուտք";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // username
             // 
-            this.username.Location = new System.Drawing.Point(111, 60);
+            this.username.Location = new System.Drawing.Point(112, 17);
             this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(149, 20);
+            this.username.Size = new System.Drawing.Size(135, 20);
             this.username.TabIndex = 1;
-            this.username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.username.TextChanged += new System.EventHandler(this.username_TextChanged);
+            this.username.Leave += new System.EventHandler(this.usernameLeave);
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(111, 99);
+            this.password.Enabled = false;
+            this.password.Location = new System.Drawing.Point(112, 48);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(149, 20);
-            this.password.TabIndex = 1;
+            this.password.PasswordChar = '☺';
+            this.password.Size = new System.Drawing.Size(135, 20);
+            this.password.TabIndex = 2;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged_1);
+            this.password.Leave += new System.EventHandler(this.passwordLeave);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 63);
+            this.label1.Location = new System.Drawing.Point(15, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "Օգտվողի անուն";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(14, 102);
+            this.label2.Location = new System.Drawing.Point(15, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Գաղտնաբառ";
-            this.label2.Click += new System.EventHandler(this.label1_Click);
             // 
             // iAgree
             // 
             this.iAgree.AutoSize = true;
-            this.iAgree.Location = new System.Drawing.Point(46, 169);
+            this.iAgree.Enabled = false;
+            this.iAgree.Location = new System.Drawing.Point(47, 80);
             this.iAgree.Name = "iAgree";
-            this.iAgree.Size = new System.Drawing.Size(180, 17);
+            this.iAgree.Size = new System.Drawing.Size(168, 17);
             this.iAgree.TabIndex = 3;
-            this.iAgree.Text = "Ծանոթացել եմ պայմաններին։";
+            this.iAgree.Text = "Ընդունում եմ պայմանները։";
             this.iAgree.UseVisualStyleBackColor = true;
             this.iAgree.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
-            // button2
+            // usernameOK
             // 
-            this.button2.Location = new System.Drawing.Point(12, 226);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.usernameOK.Image = global::WindowsFormsApp1.Properties.Resources.green;
+            this.usernameOK.Location = new System.Drawing.Point(253, 20);
+            this.usernameOK.Name = "usernameOK";
+            this.usernameOK.Size = new System.Drawing.Size(15, 15);
+            this.usernameOK.TabIndex = 5;
+            this.usernameOK.TabStop = false;
+            this.usernameOK.Visible = false;
             // 
-            // login
+            // passwordOK
+            // 
+            this.passwordOK.Image = global::WindowsFormsApp1.Properties.Resources.green;
+            this.passwordOK.Location = new System.Drawing.Point(253, 51);
+            this.passwordOK.Name = "passwordOK";
+            this.passwordOK.Size = new System.Drawing.Size(15, 15);
+            this.passwordOK.TabIndex = 5;
+            this.passwordOK.TabStop = false;
+            this.passwordOK.Visible = false;
+            // 
+            // usernameWrong
+            // 
+            this.usernameWrong.Image = global::WindowsFormsApp1.Properties.Resources.red;
+            this.usernameWrong.Location = new System.Drawing.Point(253, 20);
+            this.usernameWrong.Name = "usernameWrong";
+            this.usernameWrong.Size = new System.Drawing.Size(15, 15);
+            this.usernameWrong.TabIndex = 5;
+            this.usernameWrong.TabStop = false;
+            this.usernameWrong.Visible = false;
+            // 
+            // passwordWrong
+            // 
+            this.passwordWrong.Image = global::WindowsFormsApp1.Properties.Resources.red;
+            this.passwordWrong.Location = new System.Drawing.Point(253, 51);
+            this.passwordWrong.Name = "passwordWrong";
+            this.passwordWrong.Size = new System.Drawing.Size(15, 15);
+            this.passwordWrong.TabIndex = 5;
+            this.passwordWrong.TabStop = false;
+            this.passwordWrong.Visible = false;
+            // 
+            // loginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.passwordWrong);
+            this.Controls.Add(this.usernameWrong);
+            this.Controls.Add(this.passwordOK);
+            this.Controls.Add(this.usernameOK);
             this.Controls.Add(this.iAgree);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.password);
             this.Controls.Add(this.username);
             this.Controls.Add(this.button1);
-            this.Name = "login";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "loginForm";
             this.Text = "Մուտք";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usernameOK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordOK)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usernameWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.passwordWrong)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox username;
         private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.CheckBox iAgree;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox usernameOK;
+        private System.Windows.Forms.PictureBox passwordOK;
+        private System.Windows.Forms.PictureBox usernameWrong;
+        private System.Windows.Forms.PictureBox passwordWrong;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -10,17 +10,24 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class login : Form
+    public partial class loginForm : Form
     {
-        public login()
+        public loginForm()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        internal string _username = "Rammstein";
+        internal string _password = "Awet";
+        //public List<User> usersList = new List<User>();
+        //public User newUser(string username, string password)
+        //{
+        //    var user = new User(username, password);
+        //    usersList.Add(user);            
+        //    newUser("Rammstein", "Awet");
+        //    newUser("Aram", "Kocharyan");
+        //    return user;
+        //}
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -29,34 +36,133 @@ namespace WindowsFormsApp1
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            bool checkbox = this.iAgree.Checked;
+            if (checkbox == true)
+            {
+                this.Size = new Size(300, 325);
+                this.button1.Visible = true;
+            }
+            if (checkbox == false)
+            {
+                this.Size = new Size(300, 150);
+                this.button1.Visible = false;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string username = this.username.Text;
+            string username = this.username.Text.ToLower();
             string password = this.password.Text;
             bool isChecked = this.iAgree.Checked;
 
-            if (username == "admin" && password == "admin" && isChecked)
+            if (username == _username.ToLower() && password == _password && isChecked)
             {
-                MessageBox.Show("Բարեւ, ադմին։");
+                MessageBox.Show("Բարի գալուստ։", "Մուտքը հաջողվեց", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
                 Application.Exit();
+            }
+            else if (!isChecked)
+            {
+                MessageBox.Show("Դուք չեք ընդունել պայմանները։", "Մուտքը չհաջողվեց");
             }
             else
             {
-                MessageBox.Show("Տարոն ջան, սիրուն չի։");
+                var err = MessageBox.Show("Մուտքագրված է սխալ օգտվողի անուն կամ գաղտնաբառ։", "Մուտքը չհաջողվեց",
+                    MessageBoxButtons.RetryCancel,
+                    MessageBoxIcon.Error);
+                if (err == DialogResult.Cancel)
+                {
+                    Application.Exit();
+                }
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void username_TextChanged(object sender, EventArgs e)
+        {
+            this.usernameWrong.Visible = false;
+            this.usernameOK.Visible = false;
+            if (username.Text.Length > 0)
+            {
+                this.password.Enabled = true;
+                this.iAgree.Enabled = true;
+            }
+            else
+            {
+                this.password.Enabled = false;
+                this.iAgree.Enabled = false;
+            }
+        }
+
+        private void usernameLeave(object sender, EventArgs e)
+        {
+            string username = this.username.Text.ToLower();
+            if (username == _username.ToLower())
+            {
+                this.usernameOK.Visible = true;
+            }
+            else
+            {
+                this.usernameWrong.Visible = true;
+                this.username.Focus();
+                this.username.SelectionStart = 0;
+                this.username.SelectionLength = username.Length;
+                MessageBox.Show("Սխալ օգտվողի անուն։");
+            }
+            //bool success = false;
+            //int count = usersList.Count;
+            //MessageBox.Show(Convert.ToString(count));
+            //for (int i = 0; i < count; i++)
+            //{
+            //    if (username == usersList[i]._username)
+            //    {
+            //        this.usernameOK.Visible = true;
+            //        success = true;
+            //    }
+            //}
+            //if (success == false)
+            //{
+            //    this.usernameWrong.Visible = true;
+            //    this.username.Focus();
+            //    this.username.SelectionStart = 0;
+            //    this.username.SelectionLength = username.Length;
+            //    MessageBox.Show("Սխալ օգտվողի անուն։");
+            //}
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void passwordLeave(object sender, EventArgs e)
         {
-            this.button1.Width += 7;
+            string password = this.password.Text;
+            if (password != _password)
+            {
+                this.passwordWrong.Visible = true;
+                MessageBox.Show("Սխալ գաղտնաբառ։");
+            }
+            else
+            {
+                this.passwordOK.Visible = true;
+            }
         }
+
+        private void password_TextChanged_1(object sender, EventArgs e)
+        {
+            this.passwordWrong.Visible = false;
+            this.passwordOK.Visible = false;
+        }
+        
     }
+    
+        //public class User
+        //{
+        //    public string _username { get; set; }
+        //    public string _password { get; set; }
+        //    public User(string username, string password)
+        //    {
+        //        _username = username;
+        //        _password = password;
+        //    }
+        //}
 }
